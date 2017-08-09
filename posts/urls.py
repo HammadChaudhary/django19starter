b5d733fd3f13cc98dd4fa325a6cc42a3dbabc3d2
HammadChaudhary <hammadchaudhary@outlook.com>
2017-08-09 11:14:12 +0500

    src>posts>url.py > Support for string view arguments to url() is deprecated FIXED

--- posts/urls.py
+++ posts/urls.py
@@ -3,15 +3,31 @@ from django.conf.urls import url
 # from contact import views as contact_view
 # from posts import views as post_view
 
-from . import views
+# from . import views
+from .views import(
+	post_list,
+	post_create,
+	post_detail,
+	post_update,
+	post_delete
+
+	)
+
+# urlpatterns = [
+#     url(r'create/^$', "posts.views.post_create"),
+#     url(r'detail/^$', "posts.views.post_detail"),
+#     # Home page for posts
+#     url(r'^$', "posts.views.post_list"),
+#     url(r'update/^$', "posts.views.post_update"),
+#     url(r'delete/^$', "posts.views.post_delete"),
+#     # url(r'^admin/', "<appname>.<module>.<function>"),
+#     # url(r'^posts/', post_view.post_home),
+# ]
 
 urlpatterns = [
-    url(r'create/^$', "posts.views.post_create"),
-    url(r'detail/^$', "posts.views.post_detail"),
-    # Home page for posts
-    url(r'^$', "posts.views.post_list"),
-    url(r'update/^$', "posts.views.post_update"),
-    url(r'delete/^$', "posts.views.post_delete"),
-    # url(r'^admin/', "<appname>.<module>.<function>"),
-    # url(r'^posts/', post_view.post_home),
+    url(r'^$', post_list),
+    url(r'^create/$', post_create),
+    url(r'^detail/$', post_detail),
+    url(r'^update/$', post_update),
+    url(r'^delete/$', post_delete),
 ]
