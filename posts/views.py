b109d3125713efa3f8d322ba1e1335d81ea6941a
hammadchaudhary <hammadchaudhary@outlook.com>
2017-08-24 18:39:18 +0500

    Section 4 Lecture 35 Instance Update View

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
 from .forms import PostForm
@@ -13,11 +13,13 @@ def post_create(request):
 		instance = form.save(commit=False)
 		print(form.cleaned_data.get("title"))
 		instance.save()
+		# Message Success
+		return HttpResponseRedirect(instance.get_absolute_url())
 
 # Bad Practice - Not recommended
-	if request.method == "POST":
-		print(request.POST.get("title"))
-		print(request.POST.get("content"))
+	# if request.method == "POST":
+	# 	print(request.POST.get("title"))
+	# 	print(request.POST.get("content"))
 		# title = request.POST.get("title")
 		# content = request.POST.get("title")
 		# Post.objects.create(title=title)
@@ -63,8 +65,24 @@ def post_list(request):
 
 	return render(request, "index.html", my_context_data)
 
-def post_update(request):
-	return HttpResponse("<h1>Update</h1>")
+def post_update(request, id=None):
+
+	instance = get_object_or_404(Post, id=id)
+
+	form = PostForm(request.POST or None, instance = instance)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		print(form.cleaned_data.get("title"))
+		instance.save()
+		# Message Success
+		return HttpResponseRedirect(instance.get_absolute_url())
+
+	my_context_data = {
+	"title": "Detail",
+	"instance": instance,
+	"form": form,
+	}
+	return render(request, "post_form.html", my_context_data)
 
 def post_delete(request):
 	return HttpResponse("<h1>Delete</h1>")
