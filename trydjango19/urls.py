d825a2aa5681f1438322025bd4917887a569a377
hammadchaudhary <hammadchaudhary@outlook.com>
2017-09-14 12:18:28 +0500

    setup static files - css, js, images in Django

--- trydjango19/urls.py
+++ trydjango19/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 # from contact import views as contact_view
@@ -26,3 +28,6 @@ urlpatterns = [
     # url(r'^admin/', "<appname>.<module>.<function>"),
     # url(r'^posts/', post_view.post_home),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
